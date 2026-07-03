3253f2b326b3ce7a520c3233923f03760c510fcd
Mikhail Kshevetskiy <mikhail.kshevetskiy@iopsys.eu>
2026-07-03 14:56:16 +0300

    mtd: nand: raw: sunxi: Convert to common field_{get, prep}() helpers
    Drop the driver-specific field_get() and field_prep() macros, in favor
    of the globally available variants from <linux/bitfield.h>.
    
    Signed-off-by: Mikhail Kshevetskiy <mikhail.kshevetskiy@iopsys.eu>

--- drivers/mtd/nand/raw/sunxi_nand.h
+++ drivers/mtd/nand/raw/sunxi_nand.h
@@ -24,12 +24,7 @@
 #define SUNXI_NAND_H
 
 #include <linux/bitops.h>
-
-/* non compile-time field get/prep */
-#undef field_get
-#define field_get(_mask, _reg) (((_reg) & (_mask)) >> (ffs(_mask) - 1))
-#undef field_prep
-#define field_prep(_mask, _val) (((_val) << (ffs(_mask) - 1)) & (_mask))
+#include <linux/bitfield.h>
 
 #define NFC_REG_CTL		0x0000
 #define NFC_REG_ST		0x0004
