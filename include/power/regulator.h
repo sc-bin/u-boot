sc-bin/u-boot

commit 076265b75e61f56490003639b3d0a6483a9f507f

compat: regulator: add enable/disable macros
These just wrap regulator_set_enable() and provide a closer analogue to
the Linux API.

Signed-off-by: Casey Connolly <casey.connolly@linaro.org>

include/power/regulator.h
@@ -359,6 +359,9 @@ int regulator_get_enable(struct udevice *dev);
  */
 int regulator_set_enable(struct udevice *dev, bool enable);
 
+#define regulator_enable(dev) regulator_set_enable(dev, true)
+#define regulator_disable(dev) regulator_set_enable(dev, false)
+
 /**
  * regulator_set_enable_if_allowed: set regulator enable state if allowed by
  *					regulator

commit 0f04bbf9ab3751405308f0a8aef2b6016a072b7d

power: regulator: Fix regulator_enable/disable() macros
The commit 076265b75e61 ("compat: regulator: add enable/disable macros")
added regulator_enable/disable() macros to provide a closer analogue to
the Linux API.

These new macros wrap regulator_set_enable() that may return detailed
error codes and should typically not directly be called by drivers.

Change the macros to wrap regulator_set_enable_if_allowed() that masks
error codes consumers typically do not expect to closer match the
behavior of the Linux API.

Also move the macros outside the #if #else blocks to make the macros
available when !CONFIG_IS_ENABLED(DM_REGULATOR).

Fixes: 076265b75e61 ("compat: regulator: add enable/disable macros")
Signed-off-by: Jonas Karlman <jonas@kwiboo.se>
Signed-off-by: Peng Fan <peng.fan@nxp.com>

include/power/regulator.h
@@ -371,9 +371,6 @@ int regulator_get_enable(struct udevice *dev);
  */
 int regulator_set_enable(struct udevice *dev, bool enable);
 
-#define regulator_enable(dev) regulator_set_enable(dev, true)
-#define regulator_disable(dev) regulator_set_enable(dev, false)
-
 /**
  * regulator_set_enable_if_allowed: set regulator enable state if allowed by
  *					regulator
@@ -650,5 +647,8 @@ static inline int device_get_supply_regulator(struct udevice *dev, const char *s
 	return -ENOSYS;
 }
 #endif
+
+#define regulator_enable(dev) regulator_set_enable_if_allowed(dev, true)
+#define regulator_disable(dev) regulator_set_enable_if_allowed(dev, false)
 
 #endif /* _INCLUDE_REGULATOR_H_ */