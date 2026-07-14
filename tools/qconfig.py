sc-bin/u-boot

commit ea4d6dead3728e5e7e6a2f070ca240de832bce92

moveconfig: Rename the tool to qconfig
This does not move configs anymore, but queries them, based on a database
it can build. Rename the tool to better reflect its purpose.

Signed-off-by: Simon Glass <sjg@chromium.org>

tools/qconfig.py
@@ -5,7 +5,7 @@
 #
 
 """
-Move config options from headers to defconfig files.
+Build and query a Kconfig database for boards.
 
 See doc/develop/moveconfig.rst for documentation.
 """
@@ -900,7 +900,7 @@ def do_imply_config(config_list, add_imply, imply_flags, skip_added,
     all x86 boards will have that option, avoiding adding CONFIG_CMD_IRQ to
     each of the x86 defconfig files.
 
-    This function uses the moveconfig database to find such options. It
+    This function uses the qconfig database to find such options. It
     displays a list of things that could possibly imply those in the list.
     The algorithm ignores any that start with CONFIG_TARGET since these
     typically refer to only a few defconfigs (often one). It also does not

commit ab106a8564811b0cdbcec4b2998145dc5b304783

qconfig: Rename the doc link in help text
This was missed during the renaming of the tool. Fix it.

Signed-off-by: Julien Stephan <jstephan@baylibre.com>
Fixes: ea4d6dead372 ("moveconfig: Rename the tool to qconfig")
Reviewed-by: Tom Rini <trini@konsulko.com>

tools/qconfig.py
@@ -1514,7 +1514,7 @@ def parse_args():
         cpu_count = 1
 
     epilog = '''Move config options from headers to defconfig files. See
-doc/develop/moveconfig.rst for documentation.'''
+doc/develop/qconfig.rst for documentation.'''
 
     parser = ArgumentParser(epilog=epilog)
     # Add arguments here